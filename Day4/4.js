c43ab9dcfcc4600dcaea96355a17130f15470f4f
Jengyoung <young_pallete@naver.com>
2021-02-28 15:52:59 +0900

    Day 4 - Pass The Message JavaScript Project 구현

--- Day4/4.js
+++ Day4/4.js
@@ -1,6 +1,7 @@
 const message = document.querySelector('.message');
 const startBtn = document.querySelector('.startBtn');
 const Input = document.querySelector('input');
+
 function show() {
     const inputValue = Input.value;
     message.textContent = inputValue;
@@ -13,12 +14,8 @@ function printError() {
 }
 
 startBtn.addEventListener('click', () => {
-    show();
+    Input.value ? show() : printError()
 })
-
 Input.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        if (Input.value) show();
-        else printError();
-    }
+    if (e.key === 'Enter') Input.value ? show() : printError();
 })
---
+++ Day4/README.md
@@ -0,0 +1,8 @@
+## Day 4 - Pass The Message JavaScript Project
+
+https://jsbeginners.com/pass-the-message-project/
+
+### 기능 구현
+
+1. 메시지를 저장하는 프로그램 구현.
+2. setTimeout 비동기 함수를 통한 에러 메시지 반환 후 2초 후 사라지는 event 추가.
